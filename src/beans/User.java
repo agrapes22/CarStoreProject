76c725d83efedead19d8058ff372b19e4695c6ea
Seeker-09 <creek.mundt2@gmail.com>
2021-04-04 23:57:07 -0400

    Updated user information and registration xhtml page

--- src/beans/User.java
+++ src/beans/User.java
@@ -4,11 +4,16 @@ import javax.faces.bean.ManagedBean;
 
 @ManagedBean
 public class User {
-	private String firstName, lastName;
+	private String firstName, lastName, email, address, phoneNumber, userName, passWord;
 	
 	public User() {
-		firstName = "David";
-		lastName = "Mundt";
+		firstName = "";
+		lastName = "";
+		email = "";
+		address = "";
+		phoneNumber = "";
+		userName = "";
+		passWord = "";
 	}
 
 	public String getFirstName() {
@@ -25,6 +30,46 @@ public class User {
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassWord() {
+		return passWord;
+	}
+
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
 	}
 }
 
